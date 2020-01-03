5bb692f22bff7be3dfb51664eaf3522b752e58a0
Nimesh Soneriya <sonunimesh@gmail.com>
2020-01-03 19:02:26 +0530

    updated GetCommitListComponent

--- demo/src/app/get-commit-list/get-commit-list.component.ts
+++ demo/src/app/get-commit-list/get-commit-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,OnDestroy  } from '@angular/core';
 
 import {CommitService} from '../services/commit.service';
+import {flatMap,pluck} from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-get-commit-list',
@@ -11,18 +13,21 @@ export class GetCommitListComponent implements OnInit {
 commitList: any;
   constructor(private commitservice:CommitService) { }
   private commitUrl: string = 'https://api.github.com/repos/sonunimesh/gitcommithistory/commits';
-
+private commitsubscription: Subscription;
   ngOnInit() {
    
     const commits=  this.commitservice.getCommit(this.commitUrl);
    
-    commits.subscribe((data)=>{
+   this.commitsubscription= commits.subscribe((data)=>{
         
       if(data !== undefined)
       {
       this.commitList=data;
       }    
     } )
+  }
+  ngOnDestroy(){
+    this.commitsubscription.unsubscribe();
   }
 
 }
